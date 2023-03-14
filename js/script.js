7270b936fd63277cf7b5ce673e25303191df4138
Samantha Foigel <samantha.foigel@gmail.com>
2023-03-14 16:34:41 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 (function () {
   var names = ["Yaakov", "John", "Jason", "Jen", "Paul", "Frank", "Larry", "Laura", "Paula", "Jim"];
-  for (var i = 0; i < names.length; i++) {
+  for (var i = 0; i < names.type; i++) {
   var FirstLetter = names[i].charAt(0).toLowerCase();
   if (FirstLetter === 'j') {
     byeSpeaker.speak(names[i]);
